9f724c8eef5c9670df67acabb41c35bc36f3a580
S-VIN <7530023@gmail.com>
2020-10-13 08:30:20 +0300

    init commit bug with mod

---
+++ cmake-build-debug/CMakeFiles/circleBuffer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/circleBuffer.dir/main.cpp.o"
+  "circleBuffer"
+  "circleBuffer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/circleBuffer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
